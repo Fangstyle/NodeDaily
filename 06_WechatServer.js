7584c28db9c789f57bf54cddfbb2def7b7829a17
Fangstyle <fangstyles@163.com>
2017-02-17 17:49:59 +0800

    基于socket的聊天室

---
+++ 06_WechatServer.js
@@ -0,0 +1,70 @@
+/**
+ * Created by baocheng on 2017/2/17.
+ */
+const net = require('net');
+var clients = {};
+const server = net.createServer((socket) => {
+    // 'connection' listener
+    console.log('client connected');
+    socket.on('end', () => {
+        console.log('client disconnected');
+    });
+    socket.on('data', (data) => {
+        console.log(data);
+        var clientData = JSON.parse(data.toString().trim());
+        swtichChatStyle(clientData);
+    });
+function swtichChatStyle(data){
+    console.log(data);
+    switch (data.type){
+        case "signin":
+            login(data);
+            break;
+        case "boardcast":
+            broadcastToClient(data);
+            break;
+        case "privat":
+            break;
+        default:
+            break;
+    }
+}
+function login(data) {
+    clients[data.from] = socket;
+    console.log(`Welcome ${data.from} to 8124 chatroom 当前在线${Object.keys(clients).length}`);
+}
+function broadcastToClient(signal) {
+    // console.log(signal);
+    // 肯定有用户名和消息
+    var username = signal.from;
+    var message = signal.message;
+    // 我们要发给客户端的东西
+    var send = {
+        type: signal.type,
+        from: username,
+        message: message
+    };
+    console.log("runs broadcastToEveryOne" +signal.toString());
+    /*    // 广播消息
+     for (var froms in clients) {
+     if (clients.hasOwnProperty(froms)) {
+     var server = clients[froms];
+     server.write(JSON.stringify(send));
+     }
+     }*/
+    for(var item in clients){
+        if (clients.hasOwnProperty(item)) { //filter,只输出man的私有属性
+            clients[item].write(JSON.stringify(send));
+            //console.log(clients[item]);
+        };
+    }
+}
+});
+
+server.on('error', (err) => {
+    throw err;
+});
+server.listen(8124, () => {
+    console.log('server listenting');
+});
+
---
+++ 07_WechatClient.js
@@ -0,0 +1,61 @@
+/**
+ * Created by baocheng on 2017/2/17.
+ */
+const net = require('net');
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.question('What is your name? ', (name) => {
+    name = name.trim();
+if (!name) {
+    throw new Error('没名字还出来混。。');
+}
+const client = net.connect({port: 8124}, () => {
+        var user = {
+            type: 'signin',
+            from: name
+        }
+        client.write(JSON.stringify(user));
+    console.log('connected to server!');
+});
+client.on('data', (data) => {
+    try {
+        var signal = JSON.parse(data.toString().trim());
+        var type = signal.type;
+switch (type) {
+    case 'boardcast':
+        console.log('\nboardcast[' + signal.from + ']> ' + signal.message + '\n');
+        rl.prompt();
+        break;
+    default:
+        client.write('弄啥咧！你要干的我干不了');
+        break;
+}
+} catch (error) {
+    console.log(error);
+}
+});
+
+rl.setPrompt(name + '> '); // 此时没有写到控制台
+
+rl.prompt(); // 写入控制台
+
+// 输入一行内容敲回车
+rl.on('line', (line) => {
+
+    // {"procotol":"boardcast","from":"张三","message":"弄啥咧！"}
+    var send = {
+        type: 'boardcast',
+        from: name,
+        message: line.toString().trim()
+    };
+client.write(JSON.stringify(send));
+
+rl.prompt();
+
+});
+
+client.on('end', () => {
+    console.log('disconnected from server');
+});
+});
